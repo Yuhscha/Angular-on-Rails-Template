648666839d12c7f01c3566c18bb4ade8af67ebd1
Yuhscha <yuhscha@gmail.com>
2024-01-31 01:25:01 +0900

    Angular-17.1.1&Rails-7.1.3

---
+++ rails_api/config/application.rb
@@ -0,0 +1,40 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module RailsApi
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.1
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    config.time_zone = 'Tokyo'
+
+    config.active_record.default_timezone = :local
+
+    # sidekiq setting
+    config.active_job.queue_adapter = :Sidekiq
+    # sidekiq setting for rails api only
+    config.session_store :cookie_store, key: '_interslice_session'
+
+    # config.eager_load_paths << Rails.root.join("extras")
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
+  end
+end
